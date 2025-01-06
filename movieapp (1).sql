9d127214aad3d501be67c1211979abba787e446b
Mohamed Nezhari <109751834+Nezgova@users.noreply.github.com>
2025-01-06 19:30:05 +0100

    Update movieapp (1).sql

--- movieapp (1).sql
+++ movieapp (1).sql
@@ -32,26 +32,6 @@ CREATE TABLE `favorites` (
 
 -- --------------------------------------------------------
 
--- Insert initial data for `users`
-INSERT INTO `users` (`username`, `email`, `password`, `sex`, `profile_picture`, `phone_number`, `birthday`, `created_at`) VALUES
-('testuser', 'testuser@example.com', '$2b$10$t6Kik8iut3s42jl0r0aOFu2zQXeqQUMY28WKOSFs.qT.5.y/wWgMO', 'Other', NULL, NULL, NULL, '2024-12-29 16:40:20'),
-('admin', 'admin@emsi.ma', '$2b$10$0T.fD9UwYYi3Ufda.jB5wuc/ku/Ru4Wf.TZCfe.Wg3QVEFqxbpO52', 'Other', NULL, NULL, NULL, '2024-12-29 16:40:20'),
-('nezgova', 'nez@emsi.ma', '$2b$10$OXmAevxasRWW/cPVbBmgoe8CkVMk0DZN49xsoNtA8G.moUl0XgPIG', 'Other', '/uploads/profile_pictures/1735493557681.jpg', '+212682740674', '2004-05-12', '2024-12-29 16:40:20'),
-('ilias', 'ilias@gmail.com', '$2b$10$z.avsfPmWDDV0C4yRmlEjeDgcGsXfphzaoMKVQhfWWb/ZehRDHcsG', 'Other', NULL, NULL, NULL, '2024-12-29 16:40:20'),
-('adilB', 'adilB@emsi.ma', '$2b$10$BiXdI/Tp8s5QA8Ej7uhQpuTxLaLzEQ8dBe3zQhg6VWRZRoIyA3G2G', 'Other', '/uploads/profile_pictures/1735494688278.jpeg', NULL, NULL, '2024-12-29 16:40:20'),
-('mounda', 'mouna@emsi.ma', '$2b$10$UzlflXCc/xvlQg4VSdzdIOkQJ2nBsU98sxVZ41T8.e9cPqmhw4vaa', 'Other', NULL, NULL, NULL, '2024-12-29 16:40:20'),
-('zouhair', 'zouhair@negro.com', '$2b$10$RsWclFEgLqMYPp4ah2bHd.Ztu25rcHAQAssVq2PXCWhTc/90i9T1y', 'Other', NULL, NULL, NULL, '2024-12-29 16:40:20'),
-('tester', 'tester@emsi.ma', '$2b$10$lNzTUO2KOvcgxU9aw98nbuP0b5IjmJ2e4XcohsxE82o70pdyOrbkm', 'Other', NULL, NULL, NULL, '2024-12-29 16:40:20');
-
--- Insert initial data for `favorites`
-INSERT INTO `favorites` (`user_id`, `content_id`, `media_type`) VALUES
-(5, '839033', 'movie'),
-(5, '93405', 'tv'),
-(5, '974453', 'movie'),
-(5, '1399', 'tv'),
-(5, '30984', 'tv'),
-(7, '429', 'movie');
-
 -- --------------------------------------------------------
 
 -- Auto-increment values for `favorites` and `users`
